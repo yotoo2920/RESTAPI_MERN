eab5a3a2356c493ae042731c34b003ad5ab82a55
yotoo2920 <yotoo2920@gmail.com>
2024-02-14 13:51:57 -0500

    added create, update, and delete operations

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # RESTAPI_MERN
 // On the server side:
 
-// initialize package.json using npm init.
+// initialize package.json using:
 npm init -y
 
-// install the dependencies.
+// install the dependencies:
 npm install mongodb express cors dotenv
--- server/routes/posts.mjs
+++ server/routes/posts.mjs
@@ -27,3 +27,32 @@ router.get("/:id", async (req, res) => {
     else res.send(result).status(200);
   });
 
+// Add a new document to the collection
+// Note: You can also use InsertMany to insert multiple documents at once.
+router.post("/", async (req, res) => {
+  let collection = await db.collection("posts");
+  let newDocument = req.body;
+  newDocument.date = new Date();
+  let result = await collection.insertOne(newDocument);
+  res.send(result).status(204);
+});
+
+// Update the post with a new comment
+router.patch("/comment/:id", async (req, res) => {
+  const query = { _id: ObjectId(req.params.id) };
+  const updates = {
+    $push: { comments: req.body }
+  };
+  let collection = await db.collection("posts");
+  let result = await collection.updateOne(query, updates);
+  res.send(result).status(200);
+});
+
+// Delete an entry
+router.delete("/:id", async (req, res) => {
+  const query = { _id: ObjectId(req.params.id) };
+  const collection = db.collection("posts");
+  let result = await collection.deleteOne(query);
+  res.send(result).status(200);
+});
+
